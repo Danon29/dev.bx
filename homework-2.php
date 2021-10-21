e1a480fa7b914da2ebd166c9a94b4848add5867b
Усманов Руслан <usmanovruslan83@gmail.com>
2021-10-21 16:40:44 +0200

    homework-2.2

--- homework-2.php
+++ homework-2.php
@@ -4,15 +4,22 @@ require  ('movies (1).php');
 
 echo "Hello, please enter your age: ";
 
-$input = (int)(readline());
 
-if ($input > 0)
+$input = (readline());
+$index = 1;
+if ($input >= 0)
 {
 	foreach ($movies as $element)
 	{
-		if ($element['age_restriction'] <= $input)
+		if (($element['age_restriction'] <= $input) && (is_numeric($input)))
 		{
-			echo "{$element['title']} ({$element['release_year']}), {$element['age_restriction']}+, Rating - {$element['rating']} \n";
+			echo "{$index}. {$element['title']} ({$element['release_year']}), {$element['age_restriction']}+, Rating - {$element['rating']} \n";
+			$index++;
+		}
+		else
+		{
+			echo "Wrong input";
+			break;
 		}
 	}
 }
